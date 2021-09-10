1291a8b617724af472d644ebd7c30209cf152b1c
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-10 13:07:07 +0300

    can create a UI schema

--- controller.js
+++ controller.js
@@ -21,20 +21,11 @@ const addForm = (req, res, next) => {
     try {
         const data = req.body
         //creating a JSON with the name requested
-        let file = editJsonFile(`${__dirname}/${data.form_title}.json`);
+        let UI_file = editJsonFile(`${__dirname}/${data.form_title} UIschema.json`);
+        let file = editJsonFile(`${__dirname}/${data.form_title} schema.json`);
 
-        //making the new JSON with that title
-        file.set("form_title", data.form_title)
-
-        //going through the json to figure out the data types 
-        for (let key in data) {
-            if (key != "form_title") {
-                const inner_section = data[key]
-                for (const inner_key in inner_section) {
-                    buildInnerSections(inner_key,inner_section,file)
-                }
-            }
-        }
+        const UI_schema_JSON = createUIschema(UI_file, data)//creates the UIschema
+        const schema_JSON = createSchema(file, data)//creates the schema
 
         file.save()
 
@@ -44,21 +35,82 @@ const addForm = (req, res, next) => {
     }
 }
 
+//creates the UIschema
+const createUIschema = (UI_file, data) => {
+    try {
+        UI_file.set("type", "group")
+        UI_file.set("label", data.form_title)
+        UI_file.set("elements", [])
+        //going through the JSON received in the request (data) and setting the fields
+        for (const key in data) {
+            if (key != "form_title") {
+                const inner_section = data[key]
+                const tempJSON = {
+                    "type": "control",
+                    "label": inner_section.field_name
+                }
+                UI_file.append("elements", tempJSON)
+            }
+        }
 
-const buildInnerSections=(inner_key,inner_section,file)=>{
+        UI_file.save()
+        return
+    } catch (error) {
+        if (error)
+            console.log("problem in create UI schema:\n " + error)
+    }
+}
+
+//creates the regular (not UI) schema
+const createSchema = (file,data) => {
+    try {
+        
+    } catch (error) {
+        if (error)
+            console.log("problem in create UI schema:\n " + error)
+    }
+}
+
+//A method to build the JSON that will create the forms with JSONforms
+const buildInnerSections = (inner_key, inner_section, file) => {
     switch (inner_key) {
         case "field_name":
             {
-                file.set(key + "." + inner_key, inner_section[inner_key])
+                file.set(inner_key, {})
                 break;
             }
         case "field_type":
             {
-                //put JSONforms eligable stuff here!!!!!
+
+
+
                 break
             }
-        
+        case "values":
+            {
+                //should enter values
+                break
+            }
+
     }
 }
 
+
 module.exports = { addForm, getForm }
+
+
+
+/**
+ * //making the new JSON with that title
+        file.set("form_title", data.form_title)
+
+        //going through the json to figure out the data types
+        for (let key in data) {
+            if (key != "form_title") {
+                const inner_section = data[key]
+                for (const inner_key in inner_section) {
+                    buildInnerSections(inner_key, inner_section, file)
+                }
+            }
+        }
+ */
