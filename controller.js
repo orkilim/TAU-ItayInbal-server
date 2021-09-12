7a8e9d4cd6fb17e67c71ebe04c59800bd59c0b62
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-12 11:41:29 +0300

    can create regualr schema need to fix UI schema-insert vertical layout setting

--- controller.js
+++ controller.js
@@ -60,102 +60,137 @@ const createUIschema = (UI_file, data) => {
 }
 
 //creates the regular (not UI) schema
-const createSchema = (file,data) => {
+const createSchema = (file, data) => {
     try {
-        file.set("type","object")
-        file.set("properties",{})
-        for(const key in data)
-        {
-            if(key!="form_title")
-            {
-                const inner_section=data[key]
-                const temp_inner_JSON=checkForFields(inner_section)
-                //add to bigger JSON and what not
+        file.set("type", "object")
+        file.set("properties", {})
+        for (const key in data) {
+            if (key != "form_title") {
+                const inner_section = data[key]
+                const temp_inner_JSON = checkForFields(inner_section)
+                const temp_key = "properties." + inner_section.field_name
+                file.set(temp_key, temp_inner_JSON)
             }
         }
         file.save()
         return
     } catch (error) {
         if (error)
-            console.error("problem in create schema:\n " + error)
+            console.error("problem in createSchema:\n " + error)
     }
 }
 
-
-const checkForFields=(inner_section)=>{
+//#region Field separtion logic
+const checkForFields = (inner_section) => {
     try {
-        for(const field in inner_section)
-        {
-            if(inner_section[field]=="string")//checks if the current inner section represents a string type attribute
+        for (const field in inner_section) {
+            if (inner_section["field_type"] == "string")//checks if the current inner section represents a string type attribute
             {
                 console.log("it's a string")
-                const inner_string_JSON=checkStringFields(inner_section)
+                const inner_string_JSON = checkStringFields(inner_section)
+                return inner_string_JSON
             }
-            else if(inner_section[field]=="Number"||inner_section[field]=="Integer")//checks if the current inner section represents a number type attribute
+            else if (inner_section["field_type"] == "number" || inner_section[field] == "integer")//checks if the current inner section represents a number type attribute
             {
                 console.log("it's a num")
-                const inner_num_JSON=checkNumFields()
+                const inner_num_JSON = checkNumFields(inner_section)
+                return inner_num_JSON
             }
-            else{//if not number or string-should be boolean
-                //fill boolean logic
+            else {//if not number or string-should be boolean
+                console.log("it's boolean")
+                const name = inner_section.field_name
+                const inner_bool_JSON = {
+                    "type": "boolean"
+                }
+                return inner_bool_JSON
             }
         }
     } catch (error) {
-        if(error)
-            console.error("error in checkForFields:\n"+error)
+        if (error)
+            console.error("error in checkForFields:\n" + error)
     }
 }
 
-//takes the inner_section fields and returns a json
-const checkStringFields=(inner_section)=>{
+//takes the inner_section fields and returns a json in case of string attribute
+const checkStringFields = (inner_section) => {
     try {
-        const fieldsArr=[]
-        
-        if("minLength" in inner_section)
-        {
-            fieldsArr.push('"minLength":'+inner_section.minLength)
+        const fieldsArr = []
+
+        if ("minLength" in inner_section) {
+            fieldsArr.push('"minLength":' + inner_section.minLength)
         }
-        if("maxLength" in inner_section)
-        {
-            fieldsArr.push('"maxLength":'+inner_section.maxLength)
+        if ("maxLength" in inner_section) {
+            fieldsArr.push('"maxLength":' + inner_section.maxLength)
         }
-        if("description" in inner_section)
-        {
-            fieldsArr.push('"description":'+inner_section.description)
+        if ("description" in inner_section) {
+            fieldsArr.push('"description":' + inner_section.description)
         }
-        if("date" in inner_section)
-        {
-            fieldsArr.push('"date":'+inner_section.date)
+        if ("date" in inner_section) {
+            fieldsArr.push('"date":' + inner_section.date)
         }
-        if("values" in inner_section)
-        {
-            fieldsArr.push('"values":'+inner_section.values)
+        if ("values" in inner_section) {
+            fieldsArr.push('"values":' + inner_section.values)
         }
         fieldsArr.push("}")
-        
-        let my_JSON="{"
 
-        for(let i=0;i<fieldsArr.length-1;i++)
-        {
-            if(fieldsArr[i]!=null)
-            {
-                my_JSON+=fieldsArr[i]
+        let my_JSON = '{"type":"string",'//the string that will later become the JSON we send back
+
+        for (let i = 0; i < fieldsArr.length - 1; i++) {
+            if (fieldsArr[i] != null) {
+                my_JSON += fieldsArr[i]
             }
-            if(fieldsArr[i+1]!=null&&i!=fieldsArr.length-2)
-            {
-                my_JSON+=","
+            if (fieldsArr[i + 1] != null && i != fieldsArr.length - 2) {
+                my_JSON += ","
             }
         }
-        my_JSON+="}"
-        console.log("nigga:\n"+my_JSON)
-        my_JSON=JSON.parse(my_JSON)
-        
-        
+        my_JSON += "}"
+        my_JSON = JSON.parse(my_JSON)
+        return my_JSON
     } catch (error) {
-        if(error)
-        console.error("problem with checkStringFields:\n"+error)
+        if (error)
+            console.error("problem with checkStringFields:\n" + error)
     }
 }
+//checks the field in case of a number or integer attribute and returns a json
+const checkNumFields = (inner_section) => {
+    try {
+        const fieldsArr = []
+
+        if ("description" in inner_section) {
+            fieldsArr.push('"description":' + inner_section.description)
+        }
+        if ("maximum" in inner_section) {
+            fieldsArr.push('"maximum":' + inner_section.maximum)
+        }
+        if ("minimum" in inner_section) {
+            fieldsArr.push('"minimum":' + inner_section.minimum)
+        }
+        if ("default" in inner_section) {
+            fieldsArr.push('"default":' + inner_section.default)
+        }
+
+        fieldsArr.push("}")
+
+        let my_JSON = '{"type":"number",'//the string that will later become the JSON we send back
+
+        for (let i = 0; i < fieldsArr.length - 1; i++) {
+            if (fieldsArr[i] != null) {
+                my_JSON += fieldsArr[i]
+            }
+            if (fieldsArr[i + 1] != null && i != fieldsArr.length - 2) {
+                my_JSON += ","
+            }
+        }
+        my_JSON += "}"
+        my_JSON = JSON.parse(my_JSON)
+        return my_JSON
+    } catch (error) {
+        if (error)
+            console.error("error in checkNumFields: " + error)
+    }
+}
+
+//#endregion
 
 
 module.exports = { addForm, getForm }
@@ -178,28 +213,28 @@ module.exports = { addForm, getForm }
  */
 
 //A method to build the JSON that will create the forms with JSONforms
-        /**
-         * const buildInnerSections = (inner_key, inner_section, file) => {
-    switch (inner_key) {
-        case "field_name":
-            {
-                file.set(inner_key, {})
-                break;
-            }
-        case "field_type":
-            {
-
-
-
-                break
-            }
-        case "values":
-            {
-                //should enter values
-                break
-            }
-
+/**
+ * const buildInnerSections = (inner_key, inner_section, file) => {
+switch (inner_key) {
+case "field_name":
+    {
+        file.set(inner_key, {})
+        break;
     }
+case "field_type":
+    {
+
+
+
+        break
+    }
+case "values":
+    {
+        //should enter values
+        break
+    }
+
+}
 }
 
-         */
+ */
