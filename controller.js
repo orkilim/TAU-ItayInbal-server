e0115a59a3772a9f6836ffb71bfed7c84ed09462
or kilim <48524233+orkilim@users.noreply.github.com>
2021-09-26 18:09:08 +0300

    some changes to field separition logic were made

--- controller.js
+++ controller.js
@@ -20,7 +20,7 @@ const getForm = (req, res, next) => {
 const addForm = (req, res, next) => {
     try {
         const data = req.body
-        console.log("this is the body received: "+JSON.stringify(data))
+        console.log("this is the body received: " + JSON.stringify(data))
         //creating a JSON with the name requested
         let UI_file = editJsonFile(`${__dirname}/${data.form_title} UIschema.json`);
         let file = editJsonFile(`${__dirname}/${data.form_title} schema.json`);
@@ -28,36 +28,49 @@ const addForm = (req, res, next) => {
         const UI_schema_JSON = createUIschema(UI_file, data)//creates the UIschema
         const schema_JSON = createSchema(file, data)//creates the schema
 
-        const two_JSONs={
-            "schema":schema_JSON,
-            "UI":UI_schema_JSON
+        const two_JSONs = {
+            "schema": schema_JSON,
+            "UI": UI_schema_JSON
         }
-
+        console.log(two_JSONs)
         res.status(200).end(JSON.stringify(two_JSONs))
     } catch (err) {
-        res.status(404).send("a problem has occured with addForm action:\n"+err);
+        res.status(404).send("a problem has occured with addForm action:\n" + err);
     }
 }
 
 //creates the UIschema
 const createUIschema = (UI_file, data) => {
     try {
-        UI_file.set("type", "VerticalLayout")
-        UI_file.set("elements", [])
-        //going through the JSON received in the request (data) and setting the fields
-        for (const key in data) {
-            if (key != "form_title") {
-                const inner_section = data[key]
-                const tempJSON = {
-                    "type": "Control",
-                    "scope":"#/properties/"+inner_section.field_name,
-                    //"label": inner_section.field_name
+        UI_file.set("type", "Group")
+        let project_title = "Form by "
+        const contactArr = data.contactInfo
+        for (let i = 0; i < contactArr.length; i++)//loop to create the project's title with the name of the researchers
+        {
+            project_title += contactArr[i].name
+            if (contactArr.length > 1) {
+                if (i < contactArr.length - 2) {
+                    project_title += ", "
                 }
-                UI_file.append("elements", tempJSON)
+                if (i == contactArr.length - 2) {
+                    project_title += " and "
+                }
             }
         }
-
-        UI_file.save()
+        UI_file.set("label", project_title)
+        UI_file.set("elements", [])
+        //going through the object received in the request (data) and setting the fields
+        const data_fields_values = data.dataFieldsValues
+        for (let i = 0; i < data_fields_values.length; i++) {
+            const inner_section = data_fields_values[i]
+            const tempJSON = {
+                "type": "Control",
+                "scope": "#/properties/" + inner_section.field_name,
+                //"label": inner_section.field_name
+            }
+            UI_file.append("elements", tempJSON)
+        }
+        //UI_file.save()
         return UI_file.get()
     } catch (error) {
         if (error)
@@ -70,15 +83,14 @@ const createSchema = (file, data) => {
     try {
         file.set("type", "object")
         file.set("properties", {})
-        for (const key in data) {
-            if (key != "form_title") {
-                const inner_section = data[key]
-                const temp_inner_JSON = checkForFields(inner_section)
-                const temp_key = "properties." + inner_section.field_name
-                file.set(temp_key, temp_inner_JSON)
-            }
+        const data_fields_values = data.dataFieldsValues
+        for (let i = 0; i < data_fields_values.length; i++) {
+            const inner_section = data_fields_values[i]
+            const temp_inner_JSON = checkForFields(inner_section)
+            const temp_key = "properties." + inner_section.field_name
+            file.set(temp_key, temp_inner_JSON)
         }
-        file.save()
+        //file.save()
         return file.get()
     } catch (error) {
         if (error)
@@ -90,18 +102,18 @@ const createSchema = (file, data) => {
 const checkForFields = (inner_section) => {
     try {
         for (const field in inner_section) {
-            if (inner_section["field_type"] == "string")//checks if the current inner section represents a string type attribute
+            if (inner_section.field_type == "text")//checks if the current inner section represents a string type attribute
             {
                 const inner_string_JSON = checkStringFields(inner_section)
                 return inner_string_JSON
             }
-            else if (inner_section["field_type"] == "number" || inner_section[field] == "integer")//checks if the current inner section represents a number type attribute
+            else if (inner_section.field_type == "number")//checks if the current inner section represents a number type attribute
             {
                 const inner_num_JSON = checkNumFields(inner_section)
                 return inner_num_JSON
             }
             else {//if not number or string-should be boolean
-                const name = inner_section.field_name
+                //const name = inner_section.field_name
                 const inner_bool_JSON = {
                     "type": "boolean"
                 }
@@ -118,24 +130,27 @@ const checkForFields = (inner_section) => {
 const checkStringFields = (inner_section) => {
     try {
         const fieldsArr = []
-
-        if ("minLength" in inner_section) {
-            fieldsArr.push('"minLength":' + inner_section.minLength)
+        if (inner_section.text_type == "Free-Text") {
+            fieldsArr.push('"description":"' + inner_section.field_name+'"')
+            if (inner_section.min_val != -1) {
+                fieldsArr.push(',"minLength":' + inner_section.min_val)
+            }
+            if (inner_section.max_val != -1) {
+                fieldsArr.push(',"maxLength":' + inner_section.max_val)
+            }
         }
-        if ("maxLength" in inner_section) {
-            fieldsArr.push('"maxLength":' + inner_section.maxLength)
+        if (inner_section.text_type=="Date") {
+            fieldsArr.push('"format":"date"')
         }
-        if ("description" in inner_section) {
-            fieldsArr.push('"description":' + inner_section.description)
-        }
-        if ("date" in inner_section) {
-            fieldsArr.push('"date":' + inner_section.date)
-        }
-        if ("values" in inner_section) {
-            fieldsArr.push('"values":' + inner_section.values)
+        if (inner_section.text_type=="Choice Menu (Dropdown)") {
+            let values_for_dropdown_menu_array=[]
+            for(let i=0;i<inner_section.dropdown_fields_values.length;i++)
+            {
+                values_for_dropdown_menu_array.push('"'+(inner_section.dropdown_fields_values[i]).value_name+'"')
+            }
+            fieldsArr.push('"enum":[' + values_for_dropdown_menu_array+"]")
         }
         fieldsArr.push("}")
-
         let my_JSON = '{"type":"string",'//the string that will later become the JSON we send back
 
         for (let i = 0; i < fieldsArr.length - 1; i++) {
@@ -147,6 +162,7 @@ const checkStringFields = (inner_section) => {
             }
         }
         my_JSON += "}"
+        //console.log("this is my json string: "+my_JSON)
         my_JSON = JSON.parse(my_JSON)
         return my_JSON
     } catch (error) {
@@ -159,18 +175,18 @@ const checkNumFields = (inner_section) => {
     try {
         const fieldsArr = []
 
-        if ("description" in inner_section) {
+        /*if ("description" in inner_section) {
             fieldsArr.push('"description":' + inner_section.description)
+        }*/
+        if (inner_section.max_val != -1) {
+            fieldsArr.push('"maximum":' + inner_section.max_val)
         }
-        if ("maximum" in inner_section) {
-            fieldsArr.push('"maximum":' + inner_section.maximum)
+        if (inner_section.min_val != -1) {
+            fieldsArr.push('"minimum":' + inner_section.min_val)
         }
-        if ("minimum" in inner_section) {
-            fieldsArr.push('"minimum":' + inner_section.minimum)
-        }
-        if ("default" in inner_section) {
+        /*if ("default" in inner_section) {
             fieldsArr.push('"default":' + inner_section.default)
-        }
+        }*/
 
         fieldsArr.push("}")
 
