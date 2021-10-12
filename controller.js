8c12b24a499d3d65fa5cb8e0c881b6a87d850a6d
or kilim <48524233+orkilim@users.noreply.github.com>
2021-10-12 20:43:31 +0300

    works pretty well probably should still fix stuff

--- controller.js
+++ controller.js
@@ -9,16 +9,22 @@ const fs = require('fs')
 
 const getForms = (req, res, next) => {
     const amount = req.query.amountOfResearchers//the amount of names of researchers in the query
+    if (amount == 0) {
+        findFormsByName(null, 0)
+
+    }
     let namesArr = []
     try {
-        for (let i = 1; i <= req.query.length; i++) {
-            namesArr.push(req.query.name + i)
+        for (let i = 1; i <= amount; i++) {
+            const nameStr = "name" + i
+            namesArr.push(req.query[nameStr])
         }
+        console.log("namesArr is: " + namesArr)
         namesArr = namesArr.sort()
         findFormsByName(namesArr, amount).then((data) => {
             console.log("blah blah")
             console.log("myForms is: ", data)
-            /*switch (data.myForms) {
+            switch (data) {
                 case 0:
                     {
                         res.status(200).send(0)//there are no files with these names
@@ -29,9 +35,9 @@ const getForms = (req, res, next) => {
                         res.status(200).send(-1)//UIschema or Schema files are missing in atleast one of the forms
                         break;
                     }
-            }*/
+            }
 
-            res.status(200).send(data)
+            res.status(200).send(data.myForms)
 
         })
             .catch((error) => {
@@ -45,7 +51,106 @@ const getForms = (req, res, next) => {
 }
 
 const findFormsByName = (namesArr, amountOFnames) => {
+    //GENERAL SEARCH FOR ALL FORMS
+    if (namesArr == null && amountOFnames == 0)//retrieve ALL projects
+    {
+        console.log("i am in 1")
+        return new Promise((resolve, reject) => {
+            let myForms = []
+            let missingSchemaFileArr = []
+            let missingUIschemaFileArr = []
+            try {
+                fs.readdir(`${__dirname}`, (err, myFiles) => {
+                    if (err) {
+                        console.log("problem with readdir in getForms in findFormsByNames: " + err)
+                        return -3//problem with readdir
+                    }
+                    let stop = false; let startIndex = 0; let endIndex = 0;
+                    while (!stop)//a loop to detemine where the form files start and where they end
+                    {
+                        if (!(myFiles[startIndex].includes("formfile"))) {
+                            startIndex++
+                            endIndex++
+                        }
+                        else if (myFiles[endIndex].includes("formfile")) {
+                            endIndex++
+                        }
+                        else {
+                            stop = true
+                        }
+
+
+                    }
+                    endIndex--
+                    console.log(startIndex, endIndex)
+                    for (let i = startIndex; i <= endIndex; i += 2)//a loop to run through all the files we got
+                    {
+                        let tempArrSchema = myFiles[i].split(" ")//the file name splited to separated words
+                        let tempArrUI = myFiles[i + 1].split(" ")
+                        if ((tempArrSchema[tempArrSchema.length - 2] == tempArrUI[tempArrUI.length - 2]) && (tempArrSchema[tempArrSchema.length - 1] == "Schema.json") && (tempArrUI[tempArrUI.length - 1] == "UIschema.json")) {
+
+                            //working on one array because they are completely similar besides the UIschema.json/Schema.json
+
+                            tempArrSchema.splice(tempArrSchema.length - 1, 1)//to remove the words UIschema.json or Schema.json
+                            tempArrSchema.splice(tempArrSchema.length - 1, 1)//to remove project's name
+
+
+                            myForms.push(myFiles[i], myFiles[i + 1])
+                        }
+
+                        //TO ENABLE THE COMMENTS AFTER THIS LINE BECAUSE THEY CHECK IF THERE IS A FILE MISSING FOR A PROJECT!!!!!
+
+                        //#region Check if a file is missing
+
+                        /*else if(tempArrSchema[tempArrSchema.length - 2] != tempArrUI[tempArrUI.length - 2]){
+                            missingSchemaFileArr.push(myFiles[i])
+                            missingUIschemaFileArr.push(myFiles[i+1])
+                        }
+                        else if(tempArrSchema[tempArrSchema.length - 1] != "Schema.json"){//if one of the conditions didn't happen
+                            missingSchemaFileArr.push(myFiles[i]) //need to create a new Schema.json for that specific project name
+                        }
+                        else if(tempArrUI[tempArrUI.length - 1] == "UIschema.json")
+                        {
+                            missingUIschemaFileArr.push(myFiles[i+1]) //need to create a new UIschema for that specific project name
+                        }*/
+                        //#endregion
+
+                    }
+
+                    //console.log("myForms: ",myForms)
+
+
+                })
+
+
+                if (myForms.length == 0) {
+                    console.log("empty")
+                    resolve(0)//in case non with these names were found
+                }
+                else if (myForms.length % 2 == 1) {
+                    console.log("uneven amount of files means something is probably missing")
+                    resolve(-1)//in case there is a missing file of UIschema or Schema in one of the forms
+                }
+                else {//need to check if there is a UIschema and Schema JSONs for each form
+                    console.log("everything's cool")
+                    const myObj = {//everything's good
+                        myForms: myForms,
+                        missingUI: missingUIschemaFileArr,
+                        missingSchema: missingSchemaFileArr
+                    }
+                    resolve(myObj)
+                }
+
+            } catch (err) {
+                res.status(404).send("a problem has occured with findFormsByName: " + err);
+            }
+        })
+    }
+
+    //************************************************************ */
+    //SEARCH BY NAME!!!!!
     return new Promise((resolve, reject) => {
+        console.log("i am in 2")
         let myForms = []
         let missingSchemaFileArr = []
         let missingUIschemaFileArr = []
@@ -87,12 +192,15 @@ const findFormsByName = (namesArr, amountOFnames) => {
                         tempArrSchema = tempArrSchema.sort()//an alphabetically sorted string array. at this point the tempArr contains only the names of the researchers
                         let j = 0;
                         let flag = true//flag to show if all the names are in current tempArr file name
+                        console.log("tempArr inside is: " + tempArrSchema)
                         for (; j < amountOFnames && flag == true; j++) {
+
                             if (!(tempArrSchema.includes(namesArr[j]))) {
                                 flag = false
                             }
                         }
-                        myForms.push(myFiles[i], myFiles[i + 1])
+                        if (flag)
+                            myForms.push(myFiles[i], myFiles[i + 1])
                     }
 
                     //TO ENABLE THE COMMENTS AFTER THIS LINE BECAUSE THEY CHECK IF THERE IS A FILE MISSING FOR A PROJECT!!!!!
@@ -122,7 +230,7 @@ const findFormsByName = (namesArr, amountOFnames) => {
                 }
                 else if (myForms.length % 2 == 1) {
                     console.log("uneven amount of files means something is probably missing")
-                     resolve(-1)//in case there is a missing file of UIschema or Schema in one of the forms
+                    resolve(-1)//in case there is a missing file of UIschema or Schema in one of the forms
                 }
                 else {//need to check if there is a UIschema and Schema JSONs for each form
                     console.log("everything's cool")
@@ -146,8 +254,8 @@ const addForm = (req, res, next) => {
         const data = req.body
         console.log("this is the body received: " + JSON.stringify(data))
         //creating a JSON with the name requested
-        let UI_file = editJsonFile(`${__dirname}/${data.form_title} UIschema.json`);
-        let file = editJsonFile(`${__dirname}/${data.form_title} schema.json`);
+        let UI_file = editJsonFile(`${__dirname}/formfile ${data.form_title} UIschema.json`);
+        let file = editJsonFile(`${__dirname}/formfile ${data.form_title} schema.json`);
 
         const UI_schema_JSON = createUIschema(UI_file, data)//creates the UIschema
         const schema_JSON = createSchema(file, data)//creates the schema
